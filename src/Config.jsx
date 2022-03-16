7da28411c13b135f2e511d6261fcc43396bf9c07
lucasbaraldi <55291901+lucasbaraldi@users.noreply.github.com>
2022-03-16 15:25:48 -0300

    Update Config.jsx

--- src/Config.jsx
+++ src/Config.jsx
@@ -1,4 +1,4 @@
 module.exports = {
     //enderecoapi : "http://localhost:3002"
-    enderecoapi : "https://pw2-2021-2-carros.herokuapp.com"
+    enderecoapi : "https://pw2-2021-2-carros-api.herokuapp.com"
 }
